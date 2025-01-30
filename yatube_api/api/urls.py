47b4dc5c63a0fc0682c70b564b8190cf5c663ea1
Ilya <lomteb38@gmail.com>
2025-01-30 16:37:56 +0700

    ex

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,7 +15,6 @@ router.register(
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    path('api/v1/api-token-auth/', 
-        views.obtain_auth_token,
-        name='url_api_token'),
+    path('api/v1/api-token-auth/', views.obtain_auth_token,
+    name='url_api_token'),
 ]
